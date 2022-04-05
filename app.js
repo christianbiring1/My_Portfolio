ae8edee70f802b1a106ad4d91efbab3560302d1c
christianbiring1 <christianbiringanine22@gmail.com>
2022-04-05 10:03:38 +0200

    Fix linters issues

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-let hamburger = document.querySelector(".hamburger");
-let logo = document.querySelector(".desk-logo");
-let navMenu = document.querySelector(".nav-menu");
-let navBar = document.querySelector(".desk-nav");
+const hamburger = document.querySelector('.hamburger');
+const logo = document.querySelector('.desk-logo');
+const navMenu = document.querySelector('.nav-menu');
+const navBar = document.querySelector('.desk-nav');
 
-let navLink = document.querySelectorAll(".desk-nav a");
+const navLink = document.querySelectorAll('.desk-nav a');
 
-hamburger.addEventListener("click", (e) => {
-  hamburger.classList.toggle("active");
-  logo.classList.toggle("active");
-  navMenu.classList.toggle("active");
-  navBar.classList.toggle("active");
-  navLink.forEach((link) => link.classList.toggle("active"));
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  logo.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  navBar.classList.toggle('active');
+  navLink.forEach((link) => link.classList.toggle('active'));
 });
 
 navLink.forEach((link) =>
-  link.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    logo.classList.remove("active");
-    navMenu.classList.remove("active");
-    navBar.classList.remove("active");
-    navLink.forEach((link) => link.classList.remove("active"));
-  })
-);
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    logo.classList.remove('active');
+    navMenu.classList.remove('active');
+    navBar.classList.remove('active');
+    navLink.forEach((link) => link.classList.remove('active'))
+  }));
