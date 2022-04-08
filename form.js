0c4dcaed012d1fed1d06ab3f16a18ef1e83bd3e1
christianbiring1 <christianbiringanine22@gmail.com>
2022-04-08 23:56:05 +0200

    Fix linters errors

--- form.js
+++ form.js
@@ -6,21 +6,21 @@ const message = document.querySelector('small');
 
 // Get the value of the inputs
 
-form.addEventListener('submit',(e) => {
+form.addEventListener('submit', (e) => {
   const emailAddress = userEmail.value;
   const errorMessage = [];
 
   if (emailAddress === '') {
     errorMessage.push('The email value must not be empty');
     message.innerHTML = errorMessage.join(', ');
-  };
+  }
   if (emailAddress !== emailAddress.toLowerCase()) {
     errorMessage.push('The email must be in lower case');
     message.innerHTML = errorMessage.join(', ');
-  };
+  }
   if (errorMessage.length > 0) {
     e.preventDefault();
     message.classList.add('error');
     message.innerHTML = errorMessage.join(', ');
-  };
+  }
 });
