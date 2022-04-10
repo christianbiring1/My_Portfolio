67c564c70bd4628871c331730a865c6ee613d596
christianbiring1 <christianbiringanine22@gmail.com>
2022-04-10 18:27:18 +0200

    Put user input in local storage

--- form.js
+++ form.js
@@ -1,13 +1,17 @@
 // Constraint form validation API
 
 const form = document.querySelector('.form-contact');
+const userName = document.querySelector('#name')
 const userEmail = document.querySelector('#email');
+const userMessage = document.querySelector('#msg');
 const message = document.querySelector('small');
 
 // Get the value of the inputs
 
 form.addEventListener('submit', (e) => {
+  const client = userName.value;
   const emailAddress = userEmail.value;
+  const interestMessage = userMessage.value;
   const errorMessage = [];
 
   if (emailAddress === '') {
@@ -23,4 +27,24 @@ form.addEventListener('submit', (e) => {
     message.classList.add('error');
     message.innerHTML = errorMessage.join(', ');
   }
+});
+/* local storage */
+// set Item in the local storage
+form.addEventListener('input', () => {
+  const data = {
+    'name': userName.value,
+    'email': userEmail.value,
+    'message': userMessage.value,
+  }
+  const dataStored = JSON.stringify(data);
+  localStorage.setItem('data', dataStored);
+  location.reload;
+});
+
+// Get item from the local storage.
+window.addEventListener('load', () => {
+  const getData = JSON.parse(localStorage.getItem('data'));
+  userName.value = getData.name;
+  userEmail.value = getData.email;
+  userMessage.value = getData.message;
 });
